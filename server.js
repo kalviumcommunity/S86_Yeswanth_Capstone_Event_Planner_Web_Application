b30658783c310f75f1bee99495252c14a3cf729c
Yeswanthpola05 <yeswanth.pola.s86@kalvium.community>
2025-05-26 21:03:29 +0530

    post API used

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.use("/", guestRoutes);
 
 // Server
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on http://localhost:${3000}`);
 });
