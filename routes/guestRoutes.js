1db641a2babab5e8e04c022b902a8f30280aa2f7
yeswanth274 <samuelpola796@gmail.com>
2025-06-18 15:44:27 +0530

    Delete routes directory

--- routes/guestRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const guestController = require('../controller/guestController');
-
-router.get('/', guestController.getGuests);
-router.post('/', guestController.addGuest);
-router.put('/:id', guestController.updateGuest);
-
-module.exports = router;
