72c581afd77eb67e938e1b6bf5bbc2b7d3814d58
yeswanth274 <samuelpola796@gmail.com>
2025-06-18 15:44:01 +0530

    Delete data directory

--- data/guestData.js
+++
@@ -1,6 +0,0 @@
-const guestData = [
-  { id: 1, name: "John Doe", status: "Confirmed", type: "Adult" },
-  { id: 2, name: "Jane Smith", status: "Pending", type: "Child" }
-];
-
-module.exports = guestData;
